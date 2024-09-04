862ea1401bc258cbac53c1fdd3bcb7cda4629e3f
codephreak <codephreak@dmg.finance>
2024-09-04 15:41:38 -0700

    Update TimeLockController.sol
    // SPDX-License-Identifier: MIT
    pragma solidity ^0.8.0;
    
    import "@openzeppelin/contracts/governance/TimelockController.sol";
    
    contract DAIOTimeLock is TimelockController {
        constructor(
            uint256 _minDelay,  // Minimum delay before a proposal can be executed
            address[] memory proposers,  // Addresses allowed to propose
            address[] memory executors  // Addresses allowed to execute
        )
            TimelockController(_minDelay, proposers, executors) 
        {}
    }

--- TimeLockController.sol
+++ TimeLockController.sol
@@ -1,1 +1,15 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract DAIOTimeLock is TimelockController {
+    constructor(
+        uint256 _minDelay,  // Minimum delay before a proposal can be executed
+        address[] memory proposers,  // Addresses allowed to propose
+        address[] memory executors  // Addresses allowed to execute
+    )
+        TimelockController(_minDelay, proposers, executors) 
+    {}
+}
