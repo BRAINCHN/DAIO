9acc2c9f413c22b6ff89d9496058b3bded4cb74b
codephreak <codephreak@dmg.finance>
2024-09-09 02:05:16 -0700

    Update DAIONFT.sol
    // SPDX-License-Identifier: MIT
    pragma solidity ^0.8.0;
    
    import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
    import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
    import "@openzeppelin/contracts/access/Ownable.sol";
    import "@openzeppelin/contracts/utils/Counters.sol";
    
    /**
     * @title DAIONFT
     * @dev This contract represents the core DAIO NFT, which can later be fractionalized for governance purposes.
     */
    contract DAIONFT is ERC721URIStorage, ERC721Enumerable, Ownable {
        using Counters for Counters.Counter;
        Counters.Counter private _tokenIds;
    
        constructor() ERC721("DAIONFT", "DAIO") {}
    
        /**
         * @dev Mint a new DAIONFT to the recipient address with a metadata URI.
         * Only the owner of the contract can mint new NFTs.
         */
        function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
            _tokenIds.increment();
            uint256 newItemId = _tokenIds.current();
            _mint(recipient, newItemId);
            _setTokenURI(newItemId, tokenURI);
    
            return newItemId;
        }
    
        /**
         * @dev Allows the owner to update the token URI if needed.
         */
        function updateTokenURI(uint256 tokenId, string memory tokenURI) public onlyOwner {
            _setTokenURI(tokenId, tokenURI);
        }
    
        // Override required functions from ERC721Enumerable
        function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
            super._beforeTokenTransfer(from, to, tokenId);
        }
    
        function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
            return super.supportsInterface(interfaceId);
        }
    }

--- DAIONFT.sol
+++ DAIONFT.sol
@@ -1,34 +1,47 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 /**
  * @title DAIONFT
  * @dev This contract represents the core DAIO NFT, which can later be fractionalized for governance purposes.
  */
-contract DAIONFT is ERC721 {
+contract DAIONFT is ERC721URIStorage, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    address public owner;
 
-    constructor() ERC721("DAIONFT", "DAIO") {
-        owner = msg.sender; // Contract creator is the owner
-    }
+    constructor() ERC721("DAIONFT", "DAIO") {}
 
     /**
-     * @dev Mint a new DAIONFT to the recipient address.
+     * @dev Mint a new DAIONFT to the recipient address with a metadata URI.
      * Only the owner of the contract can mint new NFTs.
      */
-    function mintNFT(address recipient) public returns (uint256) {
-        require(msg.sender == owner, "Only owner can mint NFTs");
-
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
+    }
+
+    /**
+     * @dev Allows the owner to update the token URI if needed.
+     */
+    function updateTokenURI(uint256 tokenId, string memory tokenURI) public onlyOwner {
+        _setTokenURI(tokenId, tokenURI);
+    }
+
+    // Override required functions from ERC721Enumerable
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
